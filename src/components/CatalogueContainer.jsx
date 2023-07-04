ff19345edcadefbbce1d75d0eeed1929e61fe855
Maurizio <mauriziodm91@gmail.com>
2023-07-04 20:27:48 -0300

    created catalogue container

---
+++ src/components/CatalogueContainer.jsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react'
+import { CarsContext } from '../context/CarsContext'
+import Card from './Card'
+import Filters from './Filters'
+import './CatalogueContainer.css'
+
+const CatalogueContainer = () => {
+  const { cars } = useContext(CarsContext)
+  return (
+    <div className='catalogue-container'>
+      <div className='cta-container'>
+        <h1>Descubrí todos los modelos</h1>
+      </div>
+      <Filters />
+      <div className='cards-container'>
+        {cars.map((car) => {
+          const { id, name, year, price, thumbnail } = car
+          return (
+            <Card
+              key={id}
+              model={name}
+              year={year}
+              price={price}
+              thumbnail={thumbnail}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default CatalogueContainer
